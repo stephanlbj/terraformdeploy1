2bcb7a48187bd7e3548dc620341abbf951147cdd
stephanjames <stephanjames055@gmail.com>
2025-09-21 21:34:02 +0000

    created the vpc module

--- env/staging.tfvars
+++ env/staging.tfvars
@@ -1,2 +1,29 @@
 aws_region      = "eu-north-1"
 project         = "nest-api"
+environment     = "staging"
+
+# Réseau
+vpc_cidr        = "10.0.0.0/16"
+public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+azs             = ["eu-north-1a", "eu-north-1b"]
+
+# Tags
+tags = {
+  Environment = "staging"
+  Project     = "nest-api"
+}
+
+# Security Groups
+sg_ingress_public = [
+  { from_port = 80,  to_port = 80,  protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
+  { from_port = 443, to_port = 443, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
+]
+
+sg_ingress_private = [
+  { from_port = 3000, to_port = 3000, protocol = "tcp", source_security_group_id = "alb_sg_placeholder" }
+]
+
+sg_egress = [
+  { from_port = 0, to_port = 0, protocol = "-1", cidr_blocks = ["0.0.0.0/0"] }
+]
---
+++ outputs.tf
@@ -0,0 +1,50 @@
+# VPC
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+# Subnets
+output "public_subnets" {
+  description = "List of public subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  description = "List of private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+# Security Groups
+output "alb_sg_id" {
+  description = "Security Group ID for ALB"
+  value       = module.vpc.alb_sg_id
+}
+
+output "ecs_sg_id" {
+  description = "Security Group ID for ECS"
+  value       = module.vpc.ecs_sg_id
+}
+
+# NAT Gateway
+output "nat_gateway_id" {
+  description = "NAT Gateway ID"
+  value       = module.vpc.nat_gateway_id
+}
+
+# Internet Gateway
+output "internet_gateway_id" {
+  description = "Internet Gateway ID"
+  value       = module.vpc.internet_gateway_id
+}
+
+# Route Tables
+output "public_route_table_id" {
+  description = "Public Route Table ID"
+  value       = module.vpc.public_route_table_id
+}
+
+output "private_route_table_id" {
+  description = "Private Route Table ID"
+  value       = module.vpc.private_route_table_id
+}
